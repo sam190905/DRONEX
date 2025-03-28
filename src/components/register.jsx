c527672af05636753b2c79deab15a0c06716cfbc
Samarth Purant <samarthpurant018@gmail.com>
2025-03-29 00:47:42 +0530

    changed register

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -18,7 +18,7 @@ function Register() {
         const formData = { username, password };
 
         try {
-            const response = await fetch("http://localhost:3000/register", {
+            const response = await fetch("https://dronex-front.onrender.com/Register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
